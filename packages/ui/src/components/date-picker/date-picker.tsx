b7c9b36dd91cc7b551179544a4c187832fcda076
Lucas Thevenet Cervantes <lthevenetcervantes@santandertecnologia.com.ar>
2023-05-27 13:07:39 -0300

    fix datepickers

--- packages/ui/src/components/date-picker/date-picker.tsx
+++ packages/ui/src/components/date-picker/date-picker.tsx
@@ -3,38 +3,47 @@
 import * as React from "react";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
+import { type SelectSingleEventHandler } from "react-day-picker";
 
 import { cn } from "../../utils";
-import { Button } from "../button/button";
-import { Calendar } from "../calendar/calendar";
+import { Calendar, type CalendarProps } from "../calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "../popover";
 
-export function DatePicker() {
-  const [date, setDate] = React.useState<Date>();
+type DatePickerProps = Omit<
+  CalendarProps,
+  "mode" | "selected" | "onSelect" | "initialFocus"
+> & {
+  value?: Date;
+  onChange?: SelectSingleEventHandler;
+};
 
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="secondary"
-          color="gray"
+const DatePicker = React.forwardRef<HTMLButtonElement, DatePickerProps>(
+  function DatePicker({ value, onChange, className, ...props }, ref) {
+    return (
+      <Popover>
+        <PopoverTrigger
+          ref={ref}
           className={cn(
-            "w-[280px] justify-start text-left font-normal",
-            !date && "text-gray-500 dark:text-gray-500",
+            "inline-flex h-10 w-[280px] items-center justify-start rounded-md border border-gray-200 px-4 py-2 text-left text-sm font-normal ring-offset-white transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:ring-offset-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-800",
+            !value && "text-gray-500 dark:text-gray-500",
+            className,
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
-  );
-}
+          {value ? format(value, "PPP") : <span>Pick a date</span>}
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={value}
+            onSelect={onChange}
+            initialFocus
+            {...props}
+          />
+        </PopoverContent>
+      </Popover>
+    );
+  },
+);
+
+export { DatePicker };
--- packages/ui/src/components/date-range-picker/date-range-picker.tsx
+++ packages/ui/src/components/date-range-picker/date-range-picker.tsx
@@ -1,62 +1,73 @@
 "use client";
 
 import * as React from "react";
-import { addDays, format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
-import { type DateRange } from "react-day-picker";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import type { DateRange, SelectRangeEventHandler } from "react-day-picker";
 
 import { cn } from "../../utils";
-import { Button } from "../button/button";
-import { Calendar } from "../calendar/calendar";
+import { Calendar, type CalendarProps } from "../calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "../popover";
 
-export function DatePickerWithRange({
-  className,
-}: React.HTMLAttributes<HTMLDivElement>) {
-  const [date, setDate] = React.useState<DateRange | undefined>({
-    from: new Date(2022, 0, 20),
-    to: addDays(new Date(2022, 0, 20), 20),
-  });
+function getDateRangeString(date?: DateRange): string | null {
+  if (date?.from && date.to) {
+    return `${format(date.from, "LLL dd, y")} - ${format(
+      date.to,
+      "LLL dd, y",
+    )}`;
+  }
+  if (date?.from) {
+    return format(date.from, "LLL dd, y");
+  }
 
+  return null;
+}
+
+type DateRangePickerProps = Omit<
+  CalendarProps,
+  | "mode"
+  | "selected"
+  | "onSelect"
+  | "initialFocus"
+  | "numberOfMonths"
+  | "defaultMonth"
+> & {
+  value?: DateRange;
+  onChange?: SelectRangeEventHandler;
+};
+
+const DateRangePicker = React.forwardRef<
+  HTMLButtonElement,
+  DateRangePickerProps
+>(function DateRangePicker({ value, onChange, className, ...props }, ref) {
   return (
     <div className={cn("grid gap-2", className)}>
       <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant="secondary"
-            color="gray"
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-gray-500 dark:text-gray-500",
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
-              ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
+        <PopoverTrigger
+          id="date"
+          className={cn(
+            "inline-flex h-10 w-[300px] items-center justify-start rounded-md border border-gray-200 px-4 py-2 text-left text-sm font-normal ring-offset-white transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:ring-offset-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-800",
+            !value && "text-gray-500 dark:text-gray-500",
+          )}
+          ref={ref}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {getDateRangeString(value) ?? <span>Pick a date</span>}
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
           <Calendar
             initialFocus
             mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
+            defaultMonth={value?.from}
+            selected={value}
+            onSelect={onChange}
             numberOfMonths={2}
+            {...props}
           />
         </PopoverContent>
       </Popover>
     </div>
   );
-}
+});
+
+export { DateRangePicker };
